3f58bc4a44c7551103bf37c46aa8b411212cbe6c
Muhammad Ali <softeng.aliijaz@gmail.com>
2023-12-18 18:40:37 +0500

    User Profile Issue Resolved

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shopbiz_app/components/carousel_slider_component.dart';
 import 'package:shopbiz_app/components/drawer_component.dart';
@@ -22,47 +25,71 @@ class HomeScreen extends StatelessWidget {
       ///body
       body: Column(
         children: [
+          ///stream builder
+          Expanded(
+            child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(FirebaseAuth.instance.currentUser!.uid)
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  var user = snapshot.data!;
+
+                  if (snapshot.hasData) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: CachedNetworkImageProvider(
+                            user['photoURL'].toString()),
+                      ),
+                      title: Text("Welcome ${user['displayName'].toString()}"),
+                    );
+                  } else {
+                    return Center();
+                  }
+                }),
+          ),
+
           ///Carousel Slider
           carouselSliderMethod(),
 
           ///Grid view & Listview
-          Expanded(
-            child: GridView.builder(
-              itemCount: gridModel.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                /// Use modulo to cycle through colors
-                final colorValue = gridViewModelCardColors[
-                    index % gridViewModelCardColors.length];
+          GridView.builder(
+            shrinkWrap: true,
+            itemCount: gridModel.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              /// Use modulo to cycle through colors
+              final colorValue = gridViewModelCardColors[
+                  index % gridViewModelCardColors.length];
 
-                ///assigned
-                final value = gridModel[index].title.toString();
+              ///assigned
+              final value = gridModel[index].title.toString();
 
-                ///inkwell
-                return InkWell(
-                  onTap: () {
-                    navigateToScreen(context, value);
-                  },
-                  child: Card(
-                    color: colorValue,
-                    child: Center(
-                      child: Text(
-                        ///showed
-                        value,
-                        style: const TextStyle(
-                          decoration: TextDecoration.underline,
-                          fontWeight: FontWeight.bold,
-                        ),
+              ///inkwell
+              return InkWell(
+                onTap: () {
+                  navigateToScreen(context, value);
+                },
+                child: Card(
+                  color: colorValue,
+                  child: Center(
+                    child: Text(
+                      ///showed
+                      value,
+                      style: const TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                );
-              },
-            ),
+                ),
+              );
+            },
           ),
         ],
       ),
--- lib/screens/home/user_screens/profile_screen.dart
+++ lib/screens/home/user_screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -8,133 +9,33 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Profile'),
       ),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('users').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
           if (snapshot.hasData) {
             // Assuming there is only one user document, use first
-            var user = snapshot.data!.docs.first;
+            var user = snapshot.data!;
             return Column(
               children: [
                 CircleAvatar(
-                  backgroundImage: CachedNetworkImageProvider(
-                    user['photoURL'],
-                  ),
+                  backgroundImage:
+                      CachedNetworkImageProvider(user['photoURL'].toString()),
                 ),
                 Text(user['displayName'].toString()),
-                // Add other widgets as needed
               ],
             );
           } else {
-            return CircularProgressIndicator(); // or some loading indicator
+            // or some loading indicator
+            return const Center(child: CircularProgressIndicator());
           }
         },
       ),
     );
   }
-
-  // Container _profilePicture(QueryDocumentSnapshot<Object?> value) {
-  //   return Container(
-  //     child: Card(
-  //       child: Padding(
-  //         padding: const EdgeInsets.all(10.0),
-  //         child: CircleAvatar(
-  //           radius: 100,
-  //           backgroundImage: CachedNetworkImageProvider(
-  //             value['photoURL'].toString(),
-  //           ),
-  //           child: Align(
-  //             alignment: Alignment.bottomRight,
-  //             child: CircleAvatar(
-  //               child: IconButton(
-  //                 onPressed: () {},
-  //                 icon: const Icon(Icons.add_outlined),
-  //               ),
-  //             ),
-  //           ),
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // Widget _listTile(
-  //   IconData leadingIcon,
-  //   String title,
-  // ) {
-  //   return SizedBox(
-  //     child: Card(
-  //         child: ListTile(
-  //       leading: Icon(leadingIcon),
-  //       title: Text(title),
-  //     )),
-  //   );
-  // }
 }
-
-/*
-       SizedBox(
-        height: size.height,
-        width: size.width,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              ///circle avatar used for profile image
-              _profilePicture(size),
-
-              ///listtile to show informations
-              _listTile(Icons.person_outline, "Name",
-                  FirebaseAuth.instance.currentUser!.displayName ?? "User"),
-              _listTile(Icons.email_outlined, "Email",
-                  FirebaseAuth.instance.currentUser!.email.toString()),
-              _listTile(Icons.phone_outlined, "Phone",
-                  FirebaseAuth.instance.currentUser!.phoneNumber ?? "N/A"),
-
-              ///button
-              MaterialButton(
-                color: Theme.of(context).primaryColor,
-                shape: StadiumBorder(side: BorderSide.none),
-                onPressed: () {},
-                child: Center(child: Text('SAVE')),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Container _profilePicture(Size size) {
-    return Container(
-      padding: const EdgeInsets.all(10.0),
-      height: size.height * 0.250,
-      width: size.width,
-      child: CircleAvatar(
-        radius: 50,
-        backgroundColor: Colors.blue,
-        backgroundImage: NetworkImage(
-            FirebaseAuth.instance.currentUser!.photoURL ??
-                AppUtils.splashScreenBgImg),
-      ),
-    );
-  }
-
-  Widget _listTile(
-    IconData leadingIcon,
-    String title,
-    String subtitle,
-  ) {
-    return Card(
-      child: ListTile(
-        leading: Icon(leadingIcon),
-        title: Text(title),
-        subtitle: Text(subtitle),
-      ),
-    );
-  }
-}
-
-       */
